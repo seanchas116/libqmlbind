5f49c32989388f2737cea2466143d43372c92f57
Ryohei Ikegami <iofg2100@gmail.com>
2014-12-28 20:06:24 +0900

    Add test for loading component from file

--- test/component_test.cpp
+++ test/component_test.cpp
@@ -1,27 +1,35 @@
 #include "test_helper.h"
 #include <qmlbind.h>
-#include <catch.hpp>
 
 TEST_CASE("component")
 {
     auto engine = qmlbind_engine_new();
 
-    SECTION("loads a component from data")
+    SECTION("#create")
     {
-        auto data = R"QML(
-            import QtQml 2.2
-            QtObject {
-                property int foo: 124
-            }
-        )QML";
         auto component = qmlbind_component_new(engine);
-        qmlbind_component_set_data(component, data, "");
+
+        SECTION("with data")
+        {
+            auto data = R"QML(
+                import QtQml 2.2
+                QtObject {
+                    property int foo: 123
+                }
+            )QML";
+            qmlbind_component_set_data(component, data, "");
+        }
+
+        SECTION("with path")
+        {
+            qmlbind_component_load_path(component, "./fixtures/test.qml");
+        }
 
         auto obj = qmlbind_component_create(component);
         qmlbind_component_delete(component);
 
         auto foo = qmlbind_value_get_property(obj, "foo");
-        REQUIRE(qmlbind_value_get_number(foo) == 124);
+        REQUIRE(qmlbind_value_get_number(foo) == 123);
 
         qmlbind_value_delete(foo);
         qmlbind_value_delete(obj);
--- test/test_helper.h
+++ test/test_helper.h
@@ -3,6 +3,11 @@
 #include <QByteArray>
 #include <QString>
 #include <ostream>
+#include <catch.hpp>
+
+#define DESCRIBE(what) SECTION(what)
+#define CONTEXT(what) SECTION(what)
+#define IT(what) SECTION(what)
 
 inline std::ostream &operator<<(std::ostream &stream, const QByteArray &ba)
 {
