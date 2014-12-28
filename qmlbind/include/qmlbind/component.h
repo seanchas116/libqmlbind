e9154ca3d7f26b9e12b447cfd4599d97391854d2
Ryohei Ikegami <iofg2100@gmail.com>
2014-12-28 20:08:46 +0900

    Add missing QMLBIND_APIs

--- qmlbind/include/qmlbind/component.h
+++ qmlbind/include/qmlbind/component.h
@@ -6,13 +6,13 @@
 extern "C" {
 #endif
 
-qmlbind_component *qmlbind_component_new(qmlbind_engine *engine);
-void qmlbind_component_delete(qmlbind_component *component);
+QMLBIND_API qmlbind_component *qmlbind_component_new(qmlbind_engine *engine);
+QMLBIND_API void qmlbind_component_delete(qmlbind_component *component);
 
-void qmlbind_component_load_path(qmlbind_component *component, const char *path);
-void qmlbind_component_set_data(qmlbind_component *component, const char *data, const char *path);
+QMLBIND_API void qmlbind_component_load_path(qmlbind_component *component, const char *path);
+QMLBIND_API void qmlbind_component_set_data(qmlbind_component *component, const char *data, const char *path);
 
-qmlbind_value *qmlbind_component_create(qmlbind_component *component);
+QMLBIND_API qmlbind_value *qmlbind_component_create(qmlbind_component *component);
 
 #ifdef __cplusplus
 }
