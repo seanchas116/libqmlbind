081b8513ab0e7a114f1f87bdede6a2f0273201db
Florian Jacob <projects+git@florianjacob.de>
2016-04-03 17:47:53 +0200

    documentation for qmlbind_interface

--- qmlbind/include/qmlbind/interface.h
+++ qmlbind/include/qmlbind/interface.h
@@ -5,10 +5,29 @@
 extern "C" {
 #endif
 
+/*! \class qmlbind_interface
+ * \brief a wrapper around qmlbind_interface_handlers used to create new exporters.
+ *
+ * Its methods are defined in \ref interface.h.
+ */
+
+/*! \file interface.h
+ * \brief Contains all methods defined on `qmlbind_interface`.
+ */
+
+/*!
+ * \brief Creates a new `qmlbind_interface` wrapper.
+ *
+ * You can either create a single, generic one and pass that to every new \ref qmlbind_exporter_new, or create specific
+ * ones for each exporter.
+ */
 QMLBIND_API qmlbind_interface *qmlbind_interface_new(
     qmlbind_interface_handlers handlers
 );
 
+/*!
+ * \brief Destroys the `qmlbind_interface`.
+ */
 QMLBIND_API void qmlbind_interface_release(qmlbind_interface *interface);
 
 #ifdef __cplusplus
