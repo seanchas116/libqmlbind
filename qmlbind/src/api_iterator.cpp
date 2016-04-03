5193602befc5e32df35a97f7d75d22b6ad64093a
Florian Jacob <projects+git@florianjacob.de>
2016-04-03 17:51:23 +0200

    documentation & const for qmlbind_iterator

--- qmlbind/src/api_iterator.cpp
+++ qmlbind/src/api_iterator.cpp
@@ -1,7 +1,7 @@
 #include "qmlbind/iterator.h"
 #include <QJSValueIterator>
 
-qmlbind_iterator *qmlbind_iterator_new(qmlbind_value *object)
+qmlbind_iterator *qmlbind_iterator_new(const qmlbind_value *object)
 {
     return new QJSValueIterator(*object);
 }
@@ -11,12 +11,12 @@ void qmlbind_iterator_release(qmlbind_iterator *self)
     delete self;
 }
 
-qmlbind_string *qmlbind_iterator_get_key(qmlbind_iterator *self)
+qmlbind_string *qmlbind_iterator_get_key(const qmlbind_iterator *self)
 {
     return new QByteArray(self->name().toUtf8());
 }
 
-qmlbind_value *qmlbind_iterator_get_value(qmlbind_iterator *self)
+qmlbind_value *qmlbind_iterator_get_value(const qmlbind_iterator *self)
 {
     return new QJSValue(self->value());
 }
@@ -26,7 +26,7 @@ void qmlbind_iterator_next(qmlbind_iterator *self)
     self->next();
 }
 
-int qmlbind_iterator_has_next(qmlbind_iterator *self)
+int qmlbind_iterator_has_next(const qmlbind_iterator *self)
 {
     return self->hasNext();
 }
